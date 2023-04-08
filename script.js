916f96c09693c83998eb6797228ef2b9cfa54ec6
fadywadea <fwadea3@gmail.com>
2023-04-08 11:16:41 +0200

    commit

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ for(i in associative){
     console.log(i+":"+associative[i]);
 }
 
-///try and catch and finally {error handling}
+/* ///try and catch and finally {error handling}
 var myError = new Error("Custom Error");
 console.log(myError.name);
 console.log(myError.message);
@@ -131,3 +131,36 @@ else
     console.log("Done");
 }
 console.log("after finally block");
+*/
+
+///onerror event
+onerror = errorHandle;
+function errorHandle (msg,url,l,col,err){
+    console.log(msg);
+    return false;
+//true for don't show the error 
+//false for show the error 
+}
+var myError = new Error("Custom Error");
+console.log(myError.name);
+console.log(myError.message);
+///throe my error
+    var result = parseInt(prompt("enter value between 10 and 20","15"));
+if(result < 10)
+    throw "less than expected";
+else if(result > 20)
+    throw new RangeError ("out of range");
+else
+    alert("value within accepcted range");
+    console.log("inside try block");
+
+    if (e instanceof RangeError){
+        console.log("this is range error");
+        console.log(e.name);
+        console.log(e.message);
+    }
+    else if(e=="less than expected"){
+        console.log("low value");
+    }
+    console.log("catch block executed after error caught");
+    console.log("Done");
